9f71f6991cab19b3970eda79aa70b6afdd62fd54
1174 <1174303234@qq.com>
2018-12-08 21:10:40 +0800

    first

---
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
@@ -0,0 +1,13 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringCloudApplication
+public class LyApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyApiGateway.class);
+    }
+}
