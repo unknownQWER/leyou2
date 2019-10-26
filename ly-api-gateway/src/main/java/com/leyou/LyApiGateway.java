9d42b80ec3f7d478e6db26d76bfefd3b1027fcef
wg <1174303234@qq.com>
2019-10-26 16:01:53 +0800

    feat: 模拟冲突

--- ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class LyApiGateway {
     public static void main(String[] args) {
         SpringApplication.run(LyApiGateway.class);
+        System.out.println("wanggang1");
         System.out.println("ceshi");
     }
 }
